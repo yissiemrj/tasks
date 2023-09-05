f9229626f2b4047f01a34bd35e34468a20dcffde
Miryam <miryam9000@EMAIL>
2023-09-05 16:16:53 -0400

    add hello world

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript. Miryam Ramirez.
+                UD CISC275 with React Hooks and TypeScript. Miryam Ramirez
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <p>Hello World</p>
         </div>
     );
 }
