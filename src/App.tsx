f92cbd45b3d9f11a517d86312586f9f52e86f93d
Miryam <miryam9000@EMAIL>
2023-09-08 17:54:33 -0400

    Added header, image, list, two columes, color in header and colums, and a button.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
@@ -12,6 +13,32 @@ function App(): JSX.Element {
                 automatically reload.
             </p>
             <p>Hello World</p>
+            <h1>Header Goes Here</h1>
+            <ul>
+                <li>Coffee</li>
+                <li>Juice</li>
+                <li> Milk tea</li>
+            </ul>
+            <Button>Button</Button>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        first column
+                        <div className="shape1"></div>
+                    </Col>
+                    <Col>
+                        second column
+                        <div className="shape2"></div>
+                    </Col>
+                </Row>
+            </Container>
+            <img
+                src="https://cdn10.bostonmagazine.com/wp-content/uploads/2014/11/cat-cafe.jpg"
+                alt="A picture of coffee cat :3"
+            />
         </div>
     );
 }
