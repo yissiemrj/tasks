bdf3506486ccc2d3e4c6f4dd3654189b8826fe11
Miryam <miryam9000@EMAIL>
2023-09-25 01:11:42 -0400

    Added new function dealing with Objects

--- src/objects.ts
+++ src/objects.ts
@@ -1,3 +1,4 @@
+import { KeyObject } from "crypto";
 import { Question, QuestionType } from "./interfaces/question";
 
 /**
@@ -10,7 +11,16 @@ export function makeBlankQuestion(
     name: string,
     type: QuestionType
 ): Question {
-    return {};
+    return {
+        id: id,
+        name: name,
+        type: type,
+        body: "",
+        expected: "",
+        options: [],
+        points: 1,
+        published: false
+    };
 }
 
 /**
@@ -21,17 +31,39 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
-    return false;
+    const expectedTrim = question.expected.trim();
+    const answerTrim = answer.trim();
+    const expectedCap = question.expected.toLowerCase();
+    const answerCap = answer.toLowerCase();
+
+    if (answerTrim === expectedTrim || answerCap === expectedCap) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 /**
  * Consumes a question and a potential `answer`, and returns whether or not
- * the `answer` is valid (but not necessarily correct). For a `short_answer_question`,
- * any answer is valid. But for a `multiple_choice_question`, the `answer` must
- * be exactly one of the options.
+ * the `answer` is valid (but not necessarily correct).
+ *
+ * For a `short_answer_question`, any answer is valid.
+ * But for a `multiple_choice_question`, the `answer` must be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    //question.options
+    let result = false;
+
+    if (question.type === "short_answer_question") {
+        result = true;
+    } else if (question.type === "multiple_choice_question") {
+        if (question.options.includes(answer)) {
+            result = true;
+        } else {
+            result = false;
+        }
+    }
+    return result;
 }
 
 /**
@@ -41,7 +73,9 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    const step1 = question.id;
+    const step2 = question.name.substring(0, 10);
+    return step1 + ": " + step2;
 }
 
 /**
@@ -62,7 +96,16 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    const step1 = "# " + question.name + "\n";
+    const step2 = question.body;
+    if (question.type === "multiple_choice_question") {
+        const extra = Object.keys(question.options)
+            .map((key) => "- " + question.options[key])
+            .join("\n");
+        return step1 + step2 + "\n" + extra;
+    } else {
+        return step1 + step2;
+    }
 }
 
 /**
@@ -70,7 +113,9 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    return question;
+    const copy = { ...question };
+    copy.name = newName;
+    return copy;
 }
 
 /**
@@ -79,35 +124,49 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    const newCopy = { ...question };
+    if (newCopy.published == true) {
+        newCopy.published = false;
+    } else {
+        newCopy.published = true;
+    }
+    return newCopy;
 }
 
 /**
  * Create a new question based on the old question, copying over its `body`, `type`,
- * `options`, `expected`, and `points` without changes. The `name` should be copied
- * over as "Copy of ORIGINAL NAME" (e.g., so "Question 1" would become "Copy of Question 1").
+ * `options`, `expected`, and `points` without changes.
+ * The `name` should be copied over as "Copy of ORIGINAL NAME"
+ * (e.g., so "Question 1" would become "Copy of Question 1").
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    const newCopy = { ...oldQuestion };
+    newCopy.name = "Copy of " + oldQuestion.name;
+    newCopy.id = id;
+    newCopy.published = false;
+    return newCopy;
 }
 
 /**
  * Return a new version of the given question, with the `newOption` added to
- * the list of existing `options`. Remember that the new Question MUST have
- * its own separate copy of the `options` list, rather than the same reference
- * to the original question's list!
+ * the list of existing `options`.
+ * Remember that the new Question MUST have its own separate copy of the `options` list,
+ * rather than the same reference to the original question's list!
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    const newVer = { ...question, options: [...question.options, newOption] };
+    return newVer;
 }
 
 /**
  * Consumes an id, name, and two questions, and produces a new question.
- * The new question will use the `body`, `type`, `options`, and `expected` of the
- * `contentQuestion`. The second question will provide the `points`.
- * The `published` status should be set to false.
+ *
+ * The new question will use the `body`, `type`, `options`, and `expected` of the `contentQuestion`.
+ *
+ * The second question will provide the `points`. The `published` status should be set to false.
+ *
  * Notice that the second Question is provided as just an object with a `points`
  * field; but the function call would be the same as if it were a `Question` type!
  */
@@ -117,5 +176,15 @@ export function mergeQuestion(
     contentQuestion: Question,
     { points }: { points: number }
 ): Question {
-    return contentQuestion;
+    const new1: Question = {
+        body: contentQuestion.body,
+        type: contentQuestion.type,
+        options: contentQuestion.options,
+        expected: contentQuestion.expected,
+        id: id,
+        name: name,
+        points: points,
+        published: false
+    };
+    return new1;
 }
