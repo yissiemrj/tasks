d099470acde94ca2ed6f6ea36d2403c8abc99c3c
Miryam <miryam9000@EMAIL>
2023-10-06 12:16:12 -0400

    add all changes

--- src/components/CycleHoliday.test.tsx
+++ src/components/CycleHoliday.test.tsx
@@ -30,6 +30,7 @@ describe("CycleHoliday Component tests", () => {
         const initialHoliday = screen.getByText(/Holiday ?[:)-](.*)/i);
         const states: string[] = [];
         for (let i = 0; i < 6; i++) {
+            //console.log(initialHoliday.textContent);
             states.push(initialHoliday.textContent || "");
             alphabetButton.click();
         }
