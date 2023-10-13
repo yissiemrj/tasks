5842ce1512be512c75e96cdd206568f72c6f6ac2
Miryam <miryam9000@EMAIL>
2023-10-13 01:26:00 -0400

    add four fix components

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -14,18 +14,18 @@ export function ChooseTeam(): JSX.Element {
     const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
-    function chooseMember() {
-        /*
-        if (!team.includes(newMember)) {
-            team.push(newMember);
+    const [newMember, setMember] = useState<string>(PEOPLE[0]);
+
+    function chooseMember(option: string) {
+        setMember(option);
+
+        if (!team.includes(option)) {
+            setTeam([...team, option]);
         }
-        */
     }
 
     function clearTeam() {
-        /*
-        team = [];
-        */
+        setTeam([]);
     }
 
     return (
@@ -36,7 +36,10 @@ export function ChooseTeam(): JSX.Element {
                     {allOptions.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={chooseMember} size="sm">
+                            <Button
+                                onClick={() => chooseMember(option)}
+                                size="sm"
+                            >
                                 {option}
                             </Button>
                         </div>
--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -7,14 +7,19 @@ function ShoveBoxButton({
 }: {
     position: number;
     setPosition: (newPosition: number) => void;
-}) {
+}): JSX.Element {
     return (
         <Button onClick={() => setPosition(4 + position)}>Shove the Box</Button>
     );
 }
 
-function MoveableBox(): JSX.Element {
-    const [position, setPosition] = useState<number>(10);
+function MoveableBox({
+    position
+}: {
+    position: number;
+    //setPosition: (newPosition: number) => void;
+}): JSX.Element {
+    //const [position, setPosition] = useState<number>(10);
     return (
         <div
             data-testid="moveable-box"
@@ -32,19 +37,24 @@ function MoveableBox(): JSX.Element {
 }
 
 export function ShoveBox(): JSX.Element {
-    const box = MoveableBox();
+    //const box = MoveableBox();
+    const [position, setPosition] = useState<number>(10); //add this
 
     return (
         <div>
             <h3>Shove Box</h3>
-            {/* <span>The box is at: {box.position}</span>
-            <div>
-                <ShoveBoxButton
-                    position={box.position}
-                    setPosition={box.setPosition}
-                ></ShoveBoxButton>
-                {box}
-            </div> */}
+            {
+                <>
+                    <span>The box is at: {position}</span>
+                    <div>
+                        <ShoveBoxButton
+                            position={position}
+                            setPosition={setPosition}
+                        ></ShoveBoxButton>
+                        <MoveableBox position={position}></MoveableBox>
+                    </div>
+                </>
+            }
         </div>
     );
 }
