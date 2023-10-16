038b7b32da2c31a271fa4f95bbd069862f2d4a6b
Miryam <miryam9000@EMAIL>
2023-10-15 20:19:08 -0400

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
 import { DoubleHalf } from "./bad-components/DoubleHalf";
-import { ChooseTeam } from "./bad-components/ChooseTeam";
 import { ColoredBox } from "./bad-components/ColoredBox";
 import { ShoveBox } from "./bad-components/ShoveBox";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
@@ -17,27 +16,15 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript. Miryam Ramirez.
-                Header Goes Here
+                UD CISC275 with React Hooks and TypeScript. Miryam Ramirez
             </header>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
-            <hr></hr>
+
+            <ShoveBox></ShoveBox>
+            <ColoredBox></ColoredBox>
+            <DoubleHalf></DoubleHalf>
             <ChooseTeam></ChooseTeam>
-            <h1>Header Goes Here</h1>
-            <ul>
-                <li>Coffee</li>
-                <li>Juice</li>
-                <li> Milk tea</li>
-            </ul>
-            <Button onClick={() => console.log("Hello World!")}>
-                Log Hello World
-            </Button>
-            <img
-                src="https://cdn10.bostonmagazine.com/wp-content/uploads/2014/11/cat-cafe.jpg"
-                alt="A picture of coffee cat :3"
-            />
-            <hr></hr>
+
             <Counter></Counter>
             <hr />
             <RevealAnswer></RevealAnswer>
